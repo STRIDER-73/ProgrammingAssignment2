1eb50df7e0aa1f0d9aed293766d24b9da3e27bfd
STRIDER-73 <ramanan.b.16@gmail.com>
2022-04-16 15:04:58 +0530

    First Commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The cachematrix.R file contains two functions, 
+## makeCacheMatrix() and cachesolve(). 
 
-## Write a short comment describing this function
+## The first function in the file, makeCacheMatrix() creates an R object 
+## that stores a matrix and its inverse. 
+
+## The second function, cacheSolve() requires an argument 
+## that is returned by makeCacheMatrix() in order to retrieve the inverse 
+## from the cached value that is stored in the 
+## makeCacheMatrix() object's environment.
+
+
+
+## makeCacheMatrix() builds a set of functions 
+## and returns the functions within a list to the parent environment. 
+## That is, " aMatrix <- makeCacheMatrix(matrix(1:4,2,2)) "
+## results in an object, aMatrix, 
+## that contains four functions: 
+## set(), get(), setinverse(), and getinverse(). 
+## It also includes the two data objects, x and inversemat.
+
 
 makeCacheMatrix <- function(x = matrix()) {
+        inversemat <- NULL
+        set <- function(y) {
+                x <<- y
+                inversemat <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inv) inversemat <<- inv
+        getinverse <- function() inversemat
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() is required to populate or retrieve 
+## the inverse from an object of type makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inversemat <- x$getinverse()
+        if(!is.null(inversemat)) {
+                message("getting cached data")
+                return(inversemat)
+        }
+        data <- x$get()
+        inversemat <- solve(data, ...)
+        x$setinverse(inversemat)
+        inversemat
 }
+
